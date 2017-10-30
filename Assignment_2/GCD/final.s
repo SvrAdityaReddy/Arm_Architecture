cae43c58816b6f54ed88c9bf806e8effab4a9c14
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2017-10-30 14:16:58 +0530

    GCD code in arm instructions updated
     GCD code in arm instructions working

--- Assignment_2/GCD/final.s
+++ Assignment_2/GCD/final.s
@@ -4,9 +4,18 @@
      EXPORT __main
 	 ENTRY 
 __main  FUNCTION		 
-        MOV r0, #0x11 ; load initial value
-        LSL r1, r0, #1 ; shift 1 bit left
-        LSL r2, r1, #1 ; shift 1 bit left
+	MOV r1, #2
+	MOV r2, #2
+loop 
+	TEQ r1, r2
+	BEQ result
+	CMP r1, r2
+	ITE GT
+	SUBGT r1, r2
+	SUBLE r2, r1
+	B loop
+result
+	MOV r3, r1 ;Output is stored in r3
 stop B stop ; stop program
      ENDFUNC
      END
