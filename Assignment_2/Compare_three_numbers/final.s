269449556fc78b2f05abf2d1ebb2aaad0cfed649
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2017-10-30 17:42:52 +0530

    Added compare three numbers folder with updated code
    code is in working state with registers renamed

---
+++ Assignment_2/Compare_three_numbers/final.s
@@ -0,0 +1,33 @@
+     PRESERVE8
+     THUMB
+     AREA     appcode, CODE, READONLY
+     EXPORT __main
+	 ENTRY 
+__main  FUNCTION
+	
+number1 RN r0
+number2 RN r1
+number3 RN r2
+	MOV number1, #1
+	MOV number2, #2
+	MOV number3, #3
+	CMP number1, number2
+	BGE branch1
+	B branch2
+branch1
+	MOV r3, number1
+	B branch3
+branch2
+	MOV r3, number2
+branch3
+	CMP r3, number3
+	BGE branch4
+	B branch5
+branch4
+	MOV r4, r3 ; r4 stores greater of all three numbers number1, number2, number3
+	B stop
+branch5
+	MOV r4, number3 ; r4 stores greater of all three numbers number1, number2, number3
+stop B stop ; stop program
+     ENDFUNC
+     END
