510b5516564cf55319c0a89367aa5a34ddf5bbde
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2017-10-30 15:22:40 +0530

    GCD code updated
    Renaming of registers is done

--- Assignment_2/GCD/final.s
+++ Assignment_2/GCD/final.s
@@ -3,19 +3,21 @@
      AREA     appcode, CODE, READONLY
      EXPORT __main
 	 ENTRY 
-__main  FUNCTION		 
-	MOV r1, #2
-	MOV r2, #2
+__main  FUNCTION	
+a	RN r1
+b	RN r2
+	MOV a, #2
+	MOV b, #8
 loop 
-	TEQ r1, r2
+	TEQ a, b
 	BEQ result
-	CMP r1, r2
+	CMP a, b
 	ITE GT
-	SUBGT r1, r2
-	SUBLE r2, r1
+	SUBGT a, b
+	SUBLE b, a
 	B loop
 result
-	MOV r3, r1 ;Output is stored in r3
+	MOV r3, a ;Output i.e gcd(a,b) is stored in r3
 stop B stop ; stop program
      ENDFUNC
      END
