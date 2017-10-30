962abae039dd39e00c38f476554ca96a5f076e3d
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2017-10-30 16:19:12 +0530

    Fibonacci code updated
    Code in working state

--- Assignment_2/Fibonacci/final.s
+++ Assignment_2/Fibonacci/final.s
@@ -3,11 +3,26 @@
      AREA     appcode, CODE, READONLY
      EXPORT __main
 	 ENTRY 
-__main  FUNCTION		 
-	MOV r1, #5
+__main  FUNCTION
+	MOV r0, #0
+	MOV r1, #1
+	MOV r2, #2
+	CMP r2, #0
+	BEQ branch1
+	SUB r2, #1
 loop
-result
-	MOV r3, r1 ;Output is stored in r3
+	CMP r2, #0
+	BEQ branch2
+	MOV r3, r1 ; r3 is temporary variable
+	ADD r1, r0
+	MOV r0, r3
+	SUB r2, #1
+	B loop
+branch1 ; if n==0
+	MOV r4, r0 ; Output is stored in r4 if n is zero
+	B stop
+branch2 ; if n!=0
+	MOV r4, r1 ; Output is stored in r4 if n is non zero
 stop B stop ; stop program
      ENDFUNC
      END
