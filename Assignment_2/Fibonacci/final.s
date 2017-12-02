2307ded99a64b8f5ff832fe76459fe1746c7723c
SvrAdityaReddy <Aditya.seelapureddy@yahoo.com>
2017-12-02 16:50:29 +0530

    fibonacci code updated to generate and store series

--- Assignment_2/Fibonacci/final.s
+++ Assignment_2/Fibonacci/final.s
@@ -12,21 +12,28 @@ number RN r2
 	MOV number, #3 ; number whose fibonacci has to be calculated
 	CMP number, #0
 	; if number is 0 branched to branch1
+	MOV r5, #0x20000000 ; initial address from which first (number+1) elements of fibonacci series is stored
+	STR first, [R5]
+	ADD r5, #4
 	BEQ branch1
 	SUB number, #1
+	STR second, [R5]
+	ADD r5, #4
 loop
 	CMP number, #0
 	BEQ branch2
 	MOV r3, second ; r3 is temporary variable
 	ADD second, first
+	STR second, [R5]
+	ADD r5, #4
 	MOV first, r3
 	SUB number, #1
 	B loop
 branch1 ; if n==0
-	MOV r4, first ; Output is stored in r4 if n is zero
+	MOV r4, first ; Output is stored in r4 if number is zero i.e fibonacci(0)
 	B stop
 branch2 ; if n!=0
-	MOV r4, second ; Output is stored in r4 if n is non zero
+	MOV r4, second ; Output is stored in r4 if number is non zero i.e fibonacci(number)
 stop B stop ; stop program
      ENDFUNC
      END
